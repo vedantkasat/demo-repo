25e3a921679f8158af682a0d4b16d7b71b6a2d52
Vedant Kasat <144933939+vedantkasat@users.noreply.github.com>
2025-01-23 12:32:22 +0530

    Update star.py

--- Activity-1/star.py
+++ Activity-1/star.py
@@ -1,4 +1,6 @@
-for i in range(1, 6):
-    for j in range(1, i):
+for i in range(1, 7):
+    for j in range(1, i+1):
         print("*", end="")
     print()
+#no error in the file
+# just increased no of rows in the star
